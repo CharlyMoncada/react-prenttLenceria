643435ae911fdff051cee35d1ac3c3f1e01035a0
Carlos Moncada <moncadacarlos89@gmail.com>
2021-01-11 18:30:29 -0300

    Added navBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const NavBar = () => {
+  return (
+    <nav className='navBar'>
+      <ul>
+        <li>
+          <a href="index.html">Home</a>
+        </li>
+        <li>
+          <a href="catalogo.html">Catalogo</a>
+        </li>
+        <li>
+          <a href="carrito.html">Carrito</a>
+        </li>
+        <li>
+          <a href="preguntasFrecuentes.html">Preguntas Frecuentes</a>
+        </li>
+        <li>
+          <a href="preguntasFrecuentes.html">Envio</a>
+        </li>
+        <li>
+          <a href="contacto.html">Contacto</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
